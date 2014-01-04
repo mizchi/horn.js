19f8e2269877896852864b9d81c471e68a2d527b
Koutarou Chikuba <miz404@gmail.com>
2014-01-04 11:08:57 +0900

    Fix

--- examples/main.coffee
+++ examples/main.coffee
@@ -1,3 +1,16 @@
+Horn.registerTemplate """
+  <div
+    data-template-name="my-status"
+    data-attrs="name, money, showAddMoney">
+
+    <span data-text="name">NO NAME</span>
+    <span data-text="money">0</span>
+    <button data-click-with-trigger="update">update</button>
+    <button data-click="toggleShowAddMoney">toggle show add money</button>
+    <button data-visible="showAddMoney" data-click="addMoney">addMoney</button>
+  </div>
+"""
+
 class Status extends Horn.View
   templateName: 'my-status'
   addMoney: ->
@@ -11,10 +24,17 @@ class StatusList extends Horn.ListView
   templateName: 'my-status-list'
 
 $ ->
-  # window.a = new MyStatus
-  # a.attach 'body'
-  # a.on 'update', -> console.log 'updated'
+  # View
+  status = new Status
+  status.name = 'foo'
+  status.money = 0
 
-  window.list = new StatusList
-  list.attach 'body'
+  status.attach 'body'
+  status.on 'update', -> console.log 'updated'
+
+  # ListView
+  list = new StatusList
   list.update [{name: 1},{name: 2},{name: 3}]
+  list.addItem {name: 4}
+  list.attach 'body'
+
--- examples/main.js
+++ examples/main.js
@@ -4,6 +4,8 @@
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
+  Horn.registerTemplate("<div\n  data-template-name=\"my-status\"\n  data-attrs=\"name, money, showAddMoney\">\n\n  <span data-text=\"name\">NO NAME</span>\n  <span data-text=\"money\">0</span>\n  <button data-click-with-trigger=\"update\">update</button>\n  <button data-click=\"toggleShowAddMoney\">toggle show add money</button>\n  <button data-visible=\"showAddMoney\" data-click=\"addMoney\">addMoney</button>\n</div>");
+
   Status = (function(_super) {
     __extends(Status, _super);
 
@@ -43,9 +45,16 @@
   })(Horn.ListView);
 
   $(function() {
-    window.list = new StatusList;
-    list.attach('body');
-    return list.update([
+    var list, status;
+    status = new Status;
+    status.name = 'foo';
+    status.money = 0;
+    status.attach('body');
+    status.on('update', function() {
+      return console.log('updated');
+    });
+    list = new StatusList;
+    list.update([
       {
         name: 1
       }, {
@@ -54,6 +63,10 @@
         name: 3
       }
     ]);
+    list.addItem({
+      name: 4
+    });
+    return list.attach('body');
   });
 
 }).call(this);
